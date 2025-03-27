2df95b97945ae76ba2dfc61f958ecea0a5b2da8f
Michel F <80367602+MrWaloo@users.noreply.github.com>
2025-03-27 22:06:08 +0100

    Update klereo.class.php
    Version mise à jour pour déclencher la traduction

--- core/class/klereo.class.php
+++ core/class/klereo.class.php
@@ -34,7 +34,7 @@ class klereo extends eqLogic {
   */
   public static $_encryptConfigKey = ['login', 'password'];
 
-  public static $_version = '1.0.1 stable';
+  public static $_version = '1.0.2 beta1';
   
   static $_WEB_VERSION = '392-W';
   static $_API_ROOT = 'https://connect.klereo.fr/php/';
